6f20d384d8af6d388139406c2bac6822b2a61282
Hari Setiaji <hari.setiaji2000@gmail.com>
2022-01-04 23:22:22 +0700

    Create reader screen layout

--- app/src/main/java/com/setianjay/academy/ui/reader/CourseReaderActivity.kt
+++ app/src/main/java/com/setianjay/academy/ui/reader/CourseReaderActivity.kt
@@ -3,10 +3,50 @@ package com.setianjay.academy.ui.reader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.setianjay.academy.R
+import com.setianjay.academy.ui.reader.content.ModuleContentFragment
+import com.setianjay.academy.ui.reader.list.ModuleListFragment
 
-class CourseReaderActivity : AppCompatActivity() {
+class CourseReaderActivity : AppCompatActivity(), CourseReaderCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_reader)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val courseId = bundle.getString(EXTRA_COURSE_ID)
+            if (courseId != null) {
+                populateFragment()
+            }
+        }
+    }
+
+    private fun populateFragment() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var fragment = supportFragmentManager.findFragmentByTag(ModuleListFragment.TAG)
+        if (fragment == null) {
+            fragment = ModuleListFragment.newInstance()
+            fragmentTransaction.add(R.id.fl_container, fragment, ModuleListFragment.TAG)
+            fragmentTransaction.addToBackStack(null)
+        }
+        fragmentTransaction.commit()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount <= 1) {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    companion object{
+        const val EXTRA_COURSE_ID = "extra_course_id"
+    }
+
+    override fun moveTo(position: Int, moduleId: String) {
+        val fragment = ModuleContentFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.fl_container, fragment, ModuleContentFragment.TAG)
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/setianjay/academy/ui/reader/list/ModuleListFragment.kt
+++ app/src/main/java/com/setianjay/academy/ui/reader/list/ModuleListFragment.kt
@@ -1,33 +1,31 @@
 package com.setianjay.academy.ui.reader.list
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.setianjay.academy.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.setianjay.academy.adapter.ModuleListAdapter
+import com.setianjay.academy.adapter.MyAdapterClickListener
+import com.setianjay.academy.data.ModuleEntity
+import com.setianjay.academy.databinding.FragmentModuleListBinding
+import com.setianjay.academy.ui.reader.CourseReaderActivity
+import com.setianjay.academy.ui.reader.CourseReaderCallback
+import com.setianjay.academy.utils.DataDummy
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ModuleListFragment : Fragment(), MyAdapterClickListener {
+    private var _binding: FragmentModuleListBinding? = null
+    private val binding get() = _binding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ModuleListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ModuleListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var adapter: ModuleListAdapter
+    private lateinit var callback: CourseReaderCallback
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = context as CourseReaderActivity
     }
 
     override fun onCreateView(
@@ -35,26 +33,40 @@ class ModuleListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_module_list, container, false)
+        _binding = FragmentModuleListBinding.inflate(layoutInflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = ModuleListAdapter(this)
+        populateRecycleView(DataDummy.generateDummyModules("a14"))
+    }
+
+    private fun populateRecycleView(modules: List<ModuleEntity>){
+        binding?.apply {
+            progressBar.visibility = View.GONE
+            adapter.setModules(modules)
+            rvModule.layoutManager = LinearLayoutManager(context)
+            rvModule.setHasFixedSize(true)
+            rvModule.adapter = adapter
+            val dividerItemDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+            rvModule.addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    override fun onItemClicked(position: Int, moduleId: String) {
+        callback.moveTo(position, moduleId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ModuleListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ModuleListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val TAG: String = ModuleListFragment::class.java.simpleName
+
+        fun newInstance(): ModuleListFragment = ModuleListFragment()
     }
 }
